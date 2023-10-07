fd6baac074b374a089e1ed5c491050a7d21c006a
NileshM2003 <142876016+NileshM2003@users.noreply.github.com>
2023-10-07 12:12:30 +0530

    Add files via upload

---
+++ module4.1/oop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class alphabelt{
+	public:
+	void get(){
+		cout<<"..........HELLO WORLD.........."<<endl;
+		
+	}
+};
+int main()
+{
+	alphabelt s1;
+	s1.get();
+	
+}
